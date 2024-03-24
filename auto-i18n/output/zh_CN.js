d70ed63f098bab5e6a567663a17c3760d6365d57
huanghu0 <1525273035@qq.com>
2024-03-24 16:04:14 +0800

    feat:babel插件案列

---
+++ auto-i18n/output/zh_CN.js
@@ -0,0 +1,11 @@
+const resource = {
+    "intl1": "title",
+    "intl2": "desc",
+    "intl3": "desc",
+    "intl4": "aaa {placeholder} bbb {placeholder} ccc",
+    "intl5": "app",
+    "intl6": "测试",
+    "intl7": "intl",
+    "intl8": "intl2"
+};
+export default resource;
